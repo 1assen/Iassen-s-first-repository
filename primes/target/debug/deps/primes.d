0124f12418d168154e18a2930a3869ba4040dff9
iassen pelev <139992462+1assen@users.noreply.github.com>
2023-07-27 22:52:11 -0400

    Delete primes/target directory

--- primes/target/debug/deps/primes.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Owner\Documents\Rust programs\primes\target\debug\deps\primes.exe: src\main.rs
-
-C:\Users\Owner\Documents\Rust programs\primes\target\debug\deps\primes.d: src\main.rs
-
-src\main.rs:
--- primes/target/debug/primes.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Owner\Documents\Rust\ programs\primes\target\debug\primes.exe: C:\Users\Owner\Documents\Rust\ programs\primes\src\main.rs
